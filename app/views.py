43f5954fafc083799affbe2c348adad85154e77a
Darrell Good <darzgood@gmail.com>
2021-01-20 16:29:35 -0500

    Removed specific url reference from old hosting site

--- app/views.py
+++ app/views.py
@@ -12,7 +12,7 @@ alphacodes = DH.buildRequiredAlphaCodeDicts(basedir+"/static/masterData2.txt")
 import sys, traceback
 import logging
 logging.basicConfig(filename='birdquiz.log',level=logging.DEBUG)
-      
+
 @app.route('/')
 def root():
     return redirect(url_for('setup'))
@@ -38,7 +38,7 @@ def test3():
 @app.route('/setup', methods=['GET', 'POST'])
 def setup():
     '''
-    Build the setup page and commits most of the quiz variables to the 
+    Build the setup page and commits most of the quiz variables to the
     data-base.
     '''
     if "quizID" in request.args: # if redirected a previous quiz, populates form with previous quiz's data
@@ -49,7 +49,7 @@ def setup():
         setupForm = HomePageForm()
 
     if setupForm.validate_on_submit():
-        
+
         try:
             photoDir = basedir+"/static/photos/"
             #logging.info("testing photosDict")
@@ -60,23 +60,23 @@ def setup():
             logging.error(traceback.format_exc())
             traceback.print_exc(file=open("birdquiz.log","a"))
             sys.exit()
-            
+
         '''photoDir = basedir+"/static/photos/"
         photos = DH.birdPhotos(DH.allImages(photoDir), alphacodes["codes2PlainNames"])
         photosDict = DH.birdPhotos2(DH.allImages(photoDir), alphacodes["codes2PlainNames"])
         logging.debug("Old photosDict: ")
         logging.debug(photosDict)'''
-        
-        
-        
-            
-        
+
+
+
+
+
         # currentImage = DH.getRandomImage(photosDict)
         # print(currentImage)
         #availablePhotosDict = DH. #Dictionary to hold photos of birds that have not been used yet.
 
         try:
-            
+
             thisquiz = models.Quiz(
                 username = setupForm.username.data,
                 timestamp = datetime.datetime.utcnow(),
@@ -94,21 +94,21 @@ def setup():
             db.session.add(thisquiz)
             DH.setRandomImage(thisquiz, photographers = {"dg": "Darrell Good", "tg":"Tom Grey"})
             db.session.commit()
-            
-            
-    
+
+
+
         except:
             logging.info("DB Failure")
             logging.error(traceback.format_exc())
             traceback.print_exc(file=open("birdquiz.log","a"))
             sys.exit()
-        
-        
+
+
         # session['basedir'] = os.path.abspath(os.path.dirname(__file__))
         # session['setup'] = True
         # session['username'] = setupForm.username.data
         # session['numberOfQuestions'] = setupForm.numberOfQuestions.data
-        # ACD2 = buildAlphaCodeDict(session['basedir'] + 
+        # ACD2 = buildAlphaCodeDict(session['basedir'] +
         # "/static/masterData2.txt", lowercase = False)
         # session['alphacodes'] = ACD
         # session['images'] = allImages(session['basedir']+"/static/photos/")
@@ -119,12 +119,12 @@ def setup():
 def question():
     quizID = request.args["quizID"]
     quiz = models.Quiz.query.get(quizID)
-    
+
     user = {'nickname': quiz.username}
-    
+
     if quiz.questionComplete == quiz.questionTotal:
         userFile = open("users.log", "a")
-        userFile.write("{0}  ID {4}({1}/{2} @{3})\n".format(quiz.username, 
+        userFile.write("{0}  ID {4}({1}/{2} @{3})\n".format(quiz.username,
         quiz.questionCorrect, quiz.questionTotal, quiz.timestamp, quizID))
         userFile.close()
 
@@ -135,23 +135,23 @@ def question():
         #logging.info("Message: ")
         #logging.info(quiz.messages[-1:])
         return redirect(url_for('finish', quizID = quizID))
-    
-    
+
+
     guessForm = GuessForm()
     if guessForm.validate_on_submit():
-        
+
         try:
-        
+
             answerMessage = DH.checkAnswer(guessForm.guess.data, quiz,
-            alphacodes["codes2PlainNames"], alphacodes["codes2FormalNames"], 
+            alphacodes["codes2PlainNames"], alphacodes["codes2FormalNames"],
             alphacodes["names2Codes"])
-        
+
         except:
             logging.info("Quiz Failure")
             logging.error(traceback.format_exc())
             traceback.print_exc(file=open("birdquiz.log","a"))
             sys.exit()
-            
+
         if answerMessage == False and quiz.retry == False:
             redoMessage = '''Sorry. I can't recognize the name "{}". Please try again.
             '''.format(guessForm.guess.data)
@@ -174,19 +174,19 @@ def question():
         photo="/static/photos/"+quiz.currentImage,
         form = guessForm, photographer = quiz.photographer,
         retry = quiz.retry, message = quiz.messages[-1])
-    
+
 @app.route('/answer', methods=['GET', 'POST'])
 def answer():
     quizID = request.args["quizID"]
     quiz = models.Quiz.query.get(quizID)
-    
+
     user = {'nickname': quiz.username}
-    
+
     answerForm = BlankForm()
     if answerForm.validate_on_submit():
         #logging.info(quiz.availablePhotosDict)
-        
-        
+
+
         quiz.retry = False
         #logging.info("Answer view test")
         DH.setRandomImage(quiz, photographers = {"dg": "Darrell Good", "tg":"Tom Grey"}) # Setting variables for next question
@@ -194,10 +194,10 @@ def answer():
         #logging.info(quiz.availablePhotosDict)
         return redirect(url_for('question', quizID = quizID))
     db.session.commit()
-    
-    try: 
+
+    try:
         return render_template('answer.html', title='Bird Quiz', user=user,
-            photo="/static/photos/"+quiz.currentImage, form = answerForm, 
+            photo="/static/photos/"+quiz.currentImage, form = answerForm,
             photographer = quiz.photographer, message = quiz.messages[-1],
             birdname = alphacodes["codes2FormalNames"][DH.getCodefromImageName(quiz.currentImage, alphacodes["codes2PlainNames"], alphacodes["names2Codes"])]) #TODO: Simplify this last statement by adding a "currentBirdCode" element to the database
     except:
@@ -205,7 +205,7 @@ def answer():
         logging.error(traceback.format_exc())
         traceback.print_exc(file=open("birdquiz.log","a"))
         sys.exit()
-    
+
 @app.route('/finsh', methods = ["GET", "POST"])
 def finish():
     quizID = request.args["quizID"]
@@ -218,42 +218,42 @@ def finish():
     return render_template("finish.html", title='Bird Quiz', user=user,
         photo="/static/photos/"+quiz.currentImage, form = againForm,
         finalbird = alphacodes["codes2FormalNames"][DH.getCodefromImageName(quiz.currentImage, alphacodes["codes2PlainNames"], alphacodes["names2Codes"])],
-        photographer = quiz.photographer, message = quiz.messages[-1])  
-        
+        photographer = quiz.photographer, message = quiz.messages[-1])
+
 @app.route('/about', methods = ["GET", "POST"])
 def about():
     try:
     	logging.info("testing")
-    	with open('/home2/crgood/birdquiz.craigood.com/cgi-bin/app/static/about.txt', 'r') as aboutFile:
+    	with open('/static/about.txt', 'r') as aboutFile:
     		about=aboutFile.read()
     	return render_template("about.html", aboutText = about )
-    
+
     except:
             logging.info("About Page Failure")
             logging.error(traceback.format_exc())
             traceback.print_exc(file=open("birdquiz.log","a"))
             sys.exit()
-            
-            
+
+
 @app.route('/contact', methods = ["GET", "POST"])
 def contact():
     try:
     	logging.info("testing")
     	return render_template("contact.html")
-    
+
     except:
             logging.info("Contact Page Failure")
             logging.error(traceback.format_exc())
             traceback.print_exc(file=open("birdquiz.log","a"))
             sys.exit()
-          
-          
+
+
 @app.route('/resources', methods = ["GET", "POST"])
 def resources():
     try:
     	logging.info("testing")
     	return render_template("resources.html")
-    
+
     except:
             logging.info("Resources Page Failure")
             logging.error(traceback.format_exc())
